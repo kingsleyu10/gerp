33789b4b51fadffb7b6630113309ff08ddaee352
Kingsley Udoyi <33712474+kingsleyu10@users.noreply.github.com>
2018-07-06 14:14:49 -0400

    Create FSTree.h

---
+++ FSTree.h
@@ -0,0 +1,40 @@
+// n-ary tree of DirNodes
+
+#include <string>
+#include "DirNode.h"
+using namespace std;
+
+// Non-memeber helper functions
+void burner(DirNode *);
+DirNode *build_tree(string);
+DirNode *pre_order_copy(DirNode *, DirNode *);
+string base_name(string const &);
+bool is_file(const char *);
+bool is_dir(const char *);
+
+#ifndef FSTREE_H
+#define FSTREE_H
+
+class FSTree {
+
+public:
+    FSTree();
+    FSTree(string);
+
+    // Rule of Three
+    FSTree(const FSTree &);
+    FSTree& operator = (const FSTree &);
+    ~FSTree();
+
+    DirNode *get_root() const;
+
+    bool is_empty() const;
+    void burn_tree();
+
+private:
+
+    DirNode *root;
+
+};
+
+#endif
